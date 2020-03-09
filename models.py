d64873cd9ecf4a4246bae0661ab1d9a8f5753c29
Kerus <gibiskusus@gmail.com>
2020-03-09 23:40:00 +0400

    added oportunity to load stored models

--- models.py
+++ models.py
@@ -1,9 +1,33 @@
-from tensorflow.keras import datasets, layers, models, optimizers, metrics
+from tensorflow.keras import datasets, layers, models, optimizers, metrics, utils
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from tensorflow.keras.utils import Sequence
+
+
+def prepare_data(dataset):
+
+    if dataset == 'Fashion Mnist':
+        fashion_mnist = datasets.fashion_mnist
+        (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+
+        X_train = X_train / 255.0
+        X_test = X_test / 255.0
+
+        class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+                       "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+    elif dataset == 'CIFAR-10':
+        cifar = datasets.cifar10
+        (X_train, y_train), (X_test, y_test) = cifar.load_data()
+        X_train = X_train / 255.0
+        X_test = X_test / 255.0
+
+    y_train = utils.to_categorical(y_train, 10)
+    y_test = utils.to_categorical(y_test, 10)
+
+    return X_train, y_train, X_test, y_test
+
 
 
 def build_simple_model(dataset='Fashion Mnist', opt='sgd', hidden=None, funcs=None, loss=None, metrics_list=None):
@@ -46,6 +70,39 @@ def build_simple_model(dataset='Fashion Mnist', opt='sgd', hidden=None, funcs=No
     return model
 
 
+def load_simple_model(model_path='', weights_path='', opt='sgd', loss=None, metrics_list=None):
+    model = models.load_model(model_path)
+    model.load_weights(weights_path)
+    loss_dict = {
+        'Categorical Crossentropy': 'categorical_crossentropy',
+        'Binary Crossentropy': 'binary_crossentropy',
+        'Categorical Hinge': 'categorical_hinge',
+        'Huber loss': 'huber_loss'
+    }
+    metrics_dict = {
+        'auc': metrics.AUC(),
+        'recall': metrics.Recall(),
+        'accuracy': metrics.CategoricalAccuracy() if loss.startswith('Categorical') else metrics.Accuracy(),
+        'precision': metrics.Precision(),
+        'categorical Hinge': metrics.CategoricalHinge(),
+        'squared Hinge': metrics.SquaredHinge(),
+        'Kullback-Leibler divergence': metrics.KLDivergence(),
+        'mean absolute error': metrics.MeanAbsoluteError(),
+        'mean squared error': metrics.MeanSquaredError()
+    }
+    if metrics_list is not None and len(metrics_list) > 0:
+        metrics_list = [metrics_dict.get(m, m) for m in metrics_list]
+    else:
+        metrics_list = ['accuracy']
+
+    loss_f = loss_dict.get(loss)
+
+    model.compile(loss=loss_f,
+                  optimizer=opt,
+                  metrics=metrics_list)
+    return model
+
+
 def print_model_results(model, X, y, train=True):
     if train:
         st.write('Results on train data')
@@ -55,7 +112,6 @@ def print_model_results(model, X, y, train=True):
     results = [np.round(float(res), 2) for res in results]
     metrics = dict(zip(model.metrics_names, results))
     st.write(metrics)
-    st.write(model.summary())
 
 
 def plot_model_history(history):
--- run.py
+++ run.py
@@ -1,98 +1,116 @@
 import streamlit as st
-from tensorflow.keras import datasets, layers, models, optimizers, utils
+from tensorflow.keras import datasets, layers, models, optimizers, utils, callbacks
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
+import os
 from models import *
 
 # preprocessing
+
+
+def file_selector(label='Select a file', folder_path='.'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox(label, filenames)
+    return os.path.join(folder_path, selected_filename)
+
 from sklearn.model_selection import train_test_split
 
 #data_path = st.sidebar.file_uploader('file', type='csv')
 st.title('Neuro Build')
 
-task_option = st.sidebar.radio('Choose a task', ('Dataset overview', 'Feed-forward networks',
-                                                           'Convolutional networks', 'AutoEncoders',
+task_option = st.sidebar.radio('Choose a task', ('Feed-forward networks','Convolutional networks', 'AutoEncoders',
                                                            'GAN', 'Recurrent networks'))
 
 dataset = st.selectbox('choose dataset', ('Fashion Mnist', 'CIFAR-10'))
 
 #if data_path is not None:
 #    df = st.cache(pd.read_csv)(data_path)
-if task_option == 'Dataset overview':
 
-    st.header('Dataset overview')
-    st.write('Wait for it...')
+if task_option == 'Feed-forward networks':
 
+    build_option = st.radio('Build or load model', ('Construct new model', 'Load model'))
 
-elif task_option == 'Feed-forward networks':
-
-    st.header('Construct feed-forward network')
-    st.write('input and output layers are currently set by default')
-    hidden = st.number_input('Count of hidden layers: ', value=2, max_value=10, min_value=1)
-    i_layers = dict()
-    i_act_funcs = dict()
-    st.write(
-        f'<div style="height:1px;border:solid 1px #cccccc;margin-bottom:10px;"></div>',
-        unsafe_allow_html=True
-    )
-    for i in range(hidden):
-        i_layers[i] = st.number_input('Hidden layer {:.0f}. Count of neurons: '.format(i+1),
-                                      value=300, max_value=10000, min_value=1)
-        i_act_funcs[i] = st.selectbox('Activation function of layer {:.0f}'.format(i+1),
-                                      ('ReLU', 'SELU', 'Tanh', 'Sigmoid', 'Linear'))
-
+    if build_option == 'Construct new model':
+        st.header('Construct feed-forward network')
+        st.write('input and output layers are currently set by default')
+        hidden = st.number_input('Count of hidden layers: ', value=2, max_value=10, min_value=1)
+        i_layers = dict()
+        i_act_funcs = dict()
         st.write(
-            f'<div style="height:1px;border:solid 1px #cccccc;margin-bottom:40px;"></div>',
+            f'<div style="height:1px;border:solid 1px #cccccc;margin-bottom:10px;"></div>',
             unsafe_allow_html=True
         )
-    st.write('Adjust other hyper-parameters')
-    opt = st.selectbox('Optimiser', ('sgd', 'adadelta', 'adagrad', 'adam', 'adamax'))
+        for i in range(hidden):
+            i_layers[i] = st.number_input('Hidden layer {:.0f}. Count of neurons: '.format(i+1),
+                                          value=300, max_value=10000, min_value=1)
+            i_act_funcs[i] = st.selectbox('Activation function of layer {:.0f}'.format(i+1),
+                                          ('ReLU', 'SELU', 'Tanh', 'Sigmoid', 'Linear'))
 
-    loss_f = st.selectbox('Loss function', ('Categorical Crossentropy', 'Binary Crossentropy',
-                                            'Categorical Hinge', 'Huber loss'))
-    metrics_list = st.multiselect('Metrics: ', ('accuracy', 'recall', 'precision', 'auc', 'categorical Hinge',
-                                                'squared Hinge', 'Kullback-Leibler divergence',
-                                                'mean absolute error', 'mean squared error'), ('accuracy'))
+            st.write(
+                f'<div style="height:1px;border:solid 1px #cccccc;margin-bottom:40px;"></div>',
+                unsafe_allow_html=True
+            )
+        st.write('Adjust other hyper-parameters')
+        opt = st.selectbox('Optimiser', ('sgd', 'adadelta', 'adagrad', 'adam', 'adamax'))
 
-    epochs = st.number_input('Count of Epochs: ', value=30, max_value=10000, min_value=1)
-    val_perc = st.number_input('Percent of validation data: ', value=10, max_value=70, min_value=5)
-    val_split = val_perc / 100
+        loss_f = st.selectbox('Loss function', ('Categorical Crossentropy', 'Binary Crossentropy',
+                                                'Categorical Hinge', 'Huber loss'))
+        metrics_list = st.multiselect('Metrics: ', ('accuracy', 'recall', 'precision', 'auc', 'categorical Hinge',
+                                                    'squared Hinge', 'Kullback-Leibler divergence',
+                                                    'mean absolute error', 'mean squared error'), ('accuracy'))
 
-    if st.button("Train Network"):
+        epochs = st.number_input('Count of Epochs: ', value=30, max_value=10000, min_value=1)
+        val_perc = st.number_input('Percent of validation data: ', value=10, max_value=70, min_value=5)
+        val_split = val_perc / 100
 
-        with st.spinner('network is training...'):
-            if dataset == 'Fashion Mnist':
-                fashion_mnist = datasets.fashion_mnist
-                (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+        if st.button("Train Network"):
 
-                X_train = X_train / 255.0
-                X_test = X_test / 255.0
+            with st.spinner('network is training...'):
+                X_train, y_train, X_test, y_test = prepare_data(dataset)
 
-                class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
-                               "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
-            elif dataset == 'CIFAR-10':
-                cifar = datasets.cifar10
-                (X_train, y_train), (X_test, y_test) = cifar.load_data()
-                X_train = X_train / 255.0
-                X_test = X_test / 255.0
+                model = build_simple_model(dataset=dataset, opt=opt, hidden=i_layers,
+                                           funcs=i_act_funcs, loss=loss_f, metrics_list=metrics_list)
 
-            y_train = utils.to_categorical(y_train, 10)
-            y_test = utils.to_categorical(y_test, 10)
+                checkpoint_cb = callbacks.ModelCheckpoint("models_weights/my_keras_model.h5", save_best_only=True)
+                early_stopping_cb = callbacks.EarlyStopping(patience=10, restore_best_weights=True)
+                history = model.fit(X_train, y_train, epochs=epochs, validation_split=val_split,
+                                    shuffle=True, callbacks=[checkpoint_cb, early_stopping_cb])
 
-            model = build_simple_model(dataset=dataset, opt=opt, hidden=i_layers,
-                                       funcs=i_act_funcs, loss=loss_f, metrics_list=metrics_list)
+            st.success('Training is finished')
+            model.save("models_weights/final_model.h5")
+            model.save_weights("models_weights/final_model_weights.h5")
+            st.success('Model and weights are stored')
 
-            history = model.fit(X_train, y_train, epochs=epochs, validation_split=val_split, shuffle=True)
+            plot_model_history(history)
+            st.write('Loss function: ', loss_f)
+            print_model_results(model, X_train, y_train, train=True)
+            print_model_results(model, X_test, y_test, train=False)
 
-        st.success('Training is finished')
+    elif build_option == 'Load model':
+        data_model_path = file_selector('choose a model', 'models_weights/')
+        data_weights_path = file_selector('choose a weights', 'models_weights/')
+        #data_model_path = st.file_uploader('choose a model', type='h5')
+        #data_weights_path = st.file_uploader('choose a model weights', type='h5')
 
-        plot_model_history(history)
-        st.write('Loss function: ', loss_f)
-        print_model_results(model, X_train, y_train, train=True)
-        print_model_results(model, X_test, y_test, train=False)
+        opt = st.selectbox('Optimiser', ('sgd', 'adadelta', 'adagrad', 'adam', 'adamax'))
+
+        loss_f = st.selectbox('Loss function', ('Categorical Crossentropy', 'Binary Crossentropy',
+                                                'Categorical Hinge', 'Huber loss'))
+        metrics_list = st.multiselect('Metrics: ',
+                                      ('accuracy', 'recall', 'precision', 'auc', 'categorical Hinge',
+                                       'squared Hinge', 'Kullback-Leibler divergence',
+                                       'mean absolute error', 'mean squared error'), ('accuracy'))
+        if st.button('evaluate a model'):
+
+            model = load_simple_model(data_model_path, data_weights_path, opt, loss_f, metrics_list)
+
+            X_train, y_train, X_test, y_test = prepare_data(dataset)
+
+            print_model_results(model, X_test, y_test, train=False)
+
 
 elif task_option == 'Convolutional networks':
 
